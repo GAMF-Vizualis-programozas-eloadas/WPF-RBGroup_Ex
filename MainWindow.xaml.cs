4c9f659c00e98f25876156194ce75fdd4fc82317
jzscsaba <johanyak.csaba@nje.hu>
2025-03-01 17:29:38 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RBGroup_Example
+{
+	public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      rbSRed.Tag = Colors.Red;
+			rbSBlue.Tag = Colors.Blue;
+			rbSGreen.Tag = Colors.Green;
+			rbERed.Tag = Colors.Red;
+			rbEBlue.Tag = Colors.Blue;
+			rbEGreen.Tag = Colors.Green;
+		}
+    Color cStart = Colors.Red;
+    Color cStop = Colors.Blue;
+    private void spStartColor_Click(object sender, RoutedEventArgs e)
+    {
+      RadioButton rbSource = (RadioButton)e.Source;// as RadioButton;
+      cStart = (Color)rbSource.Tag;
+      rtSample.Fill = new LinearGradientBrush(cStart, cStop, 0.0);
+    }
+
+    private void spEndColor_Click(object sender, RoutedEventArgs e)
+    {
+      RadioButton rbSource = (RadioButton)e.Source;
+			cStop = (Color)rbSource.Tag;
+      rtSample.Fill = new LinearGradientBrush(cStart, cStop, 0.0);
+    }
+  }
+
+}
